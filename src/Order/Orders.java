6f209b8f2ff5dc4e8f1d89fb63dad08d05f77af6
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-08-24 16:00:54 +0700

    create data types, constructor and methods

--- src/Order/Orders.java
+++ src/Order/Orders.java
@@ -1,4 +1,31 @@
 package Order;
 
+import Node.*;
+import Queue.*;
+
 public class Orders {
+    private Queue<Order> orders;
+    private int quantity;
+
+    public Orders(){
+        this.orders = new Queue<Order>();
+        this.quantity = 0;
+    }
+
+    public void addOrder(Order order){
+        orders.insert(order);
+        quantity++;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void printOrders(){
+        Node<Order> current = orders.peekFront();
+        while (current != null){
+            System.out.println(current.getData().getDetails());
+            current = current.getNext();
+        }
+    }
 }
