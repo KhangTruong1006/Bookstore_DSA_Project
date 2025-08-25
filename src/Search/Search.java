89e7dfd0f13b400fcccd5369a68e266fbd358656
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-08-25 15:17:43 +0700

    rename from search to searchBooks and a new method for searching orders by customer name

--- src/Search/Search.java
+++ src/Search/Search.java
@@ -2,10 +2,12 @@ package Search;
 import List.*;
 import Books.*;
 import Node.*;
+import Order.*;
+import Queue.Queue;
 
 // Searching book will use linear search
-public class SearchBook {
-    public static SingleLinkedList<Book> search(SingleLinkedList<Book> books,String target){
+public class Search {
+    public static SingleLinkedList<Book> searchBooks(SingleLinkedList<Book> books,String target){
         Node<Book> current = books.getFirst();
         SingleLinkedList<Book> result = new SingleLinkedList<>();
         while(current != null){
@@ -27,5 +29,20 @@ public class SearchBook {
             current = current.getNext();
         }
         return null;
+    }
+
+    public static void searchOrderByName(Orders orders, String customer){
+        Node<Order> current = orders.getFirst();
+//        SingleLinkedList<Order> result = new SingleLinkedList<>();
+        while(current != null){
+            Order order = current.getData();
+            if(order.getCustomer_name().contains(customer)){
+                System.out.println(order);
+                order.printBookTitleAndAuthors();
+            }
+            current = current.getNext();
+        }
+        System.out.println("No order found");
+        return ;
     }
 }
